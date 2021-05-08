6209bb32369117bf633ad67001723a3661cf74ca
果子 <zmguozi@live.cn>
2021-05-08 09:28:30 +0800

    将帮助模版抽取到独立文件handlers/common.go

--- cmd/main.go
+++ cmd/main.go
@@ -6,20 +6,9 @@ import (
 	"iOSBox/handlers"
 
 	"github.com/gookit/gcli/v3"
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	// logrus.SetOutput(io.Discard)
-	logrus.SetLevel(logrus.DebugLevel)
-
-	gcli.AppHelpTemplate = `{{.Desc}} (版本: <info>{{.Version}}</>)
------------------------------------------------------{{range $cmdName, $c := .Cs}}
-  <info>{{$c.Name | paddingName }}</> {{$c.HelpDesc}}{{if $c.Aliases}} (别名: <green>{{ join $c.Aliases ","}}</>){{end}}{{end}}
-  <info>{{ paddingName "help" }}</> 显示帮助信息
-
-使用 "<cyan>{$binName} COMMAND -h</>" 查看命令的其他帮助信息
-`
 
 	app := gcli.NewApp(func(app *gcli.App) {
 		app.Version = "v0.1.1-alpha"
@@ -50,7 +39,6 @@ func main() {
 		handlers.ReSpringBoardCommand,
 		handlers.ForwardCommand,
 		handlers.SCPCommand,
-		handlers.DonationCommand,
 		handlers.PcapCommand,
 	)
 
